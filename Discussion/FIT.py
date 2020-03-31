365297626fb7492f3ebca4a1384bbcce31689bb6
ttnice <tranthuongtienthinh@gmail.com>
2020-03-31 21:45:50 +0200

    load for Discussion

--- Discussion/FIT.py
+++ Discussion/FIT.py
@@ -1,6 +1,6 @@
 # https://machinelearningmastery.com/text-generation-lstm-recurrent-neural-networks-python-keras/
 import numpy
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers import Dense, Dropout, LSTM, Conv1D
 from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
@@ -37,7 +37,10 @@ X = X / float(n_vocab)
 # one hot encode the output variable
 y = np_utils.to_categorical(dataY)
 
+filename = 'Models/weights-improvement-02-2.2078.hdf5'
+model = load_model(filename)
 # define the LSTM model
+'''
 model = Sequential()
 model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
 model.add(Dropout(0.2))
@@ -46,10 +49,11 @@ model.add(Dense(y.shape[1], activation='softmax'))
 model.add(LSTM(256))
 model.add(Dropout(0.2))
 model.add(Dense(y.shape[1], activation='softmax'))
+'''
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 
 # define the checkpoint
-filepath="Models/weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
+filepath="Models2/weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
 checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
 callbacks_list = [checkpoint]
 
